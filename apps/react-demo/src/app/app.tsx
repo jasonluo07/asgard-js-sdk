f4b5fbd1fca768449f2e91675a6f9db08588fd70
Ming Hsu <ming.hs.hsu@gmail.com>
2025-01-13 13:36:24 +0800

    refactor: add core, react packages and react-demo app with nx

---
+++ apps/react-demo/src/app/app.tsx
@@ -0,0 +1,44 @@
+// Uncomment this line to use CSS modules
+// import styles from './app.module.scss';
+
+import { Route, Routes, Link } from 'react-router-dom';
+import { React } from '@asgard-js/react';
+
+export function App() {
+  return (
+    <div>
+      <React />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <div>
+              This is the generated root route.{' '}
+              <Link to="/page-2">Click here for page 2.</Link>
+            </div>
+          }
+        />
+        <Route
+          path="/page-2"
+          element={
+            <div>
+              <Link to="/">Click here to go back to root page.</Link>
+            </div>
+          }
+        />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ packages/react/src/lib/react.tsx
@@ -0,0 +1,11 @@
+import styles from './react.module.scss';
+
+export function React() {
+  return (
+    <div className={styles['container']}>
+      <h1>Welcome to React!</h1>
+    </div>
+  );
+}
+
+export default React;
