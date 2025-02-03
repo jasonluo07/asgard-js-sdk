9531f38d78b758ff448f135bad806fe85a6b9d72
Ming Hsu <ming.hs.hsu@gmail.com>
2025-02-03 15:57:21 +0800

    chore(react): add @types/dom-speech-recognition, vite-plugin-svgr

--- packages/react/vite.config.ts
+++ packages/react/vite.config.ts
@@ -2,6 +2,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dts from 'vite-plugin-dts';
+import svgr from 'vite-plugin-svgr';
 import * as path from 'path';
 
 export default defineConfig({
@@ -14,6 +15,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    svgr(),
     dts({
       entryRoot: 'src',
       tsconfigPath: path.join(__dirname, 'tsconfig.lib.json'),
