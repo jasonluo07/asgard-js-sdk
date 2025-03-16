6e71da79a5b462bcce4546d3f5620d9027cd1aaf
Ming Hsu <ming.hs.hsu@gmail.com>
2025-03-16 13:54:53 +0800

    refactor(eslint): enhance ESLint configuration by adding ignored files and improving rules

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -5,11 +5,39 @@ module.exports = [
   ...nx.configs['flat/typescript'],
   ...nx.configs['flat/javascript'],
   {
-    ignores: ['**/dist'],
+    ignores: [
+      '.nx',
+      '.yarn',
+      'node_modules',
+      'vitest.workspace.ts',
+      '**/dist',
+      '**/eslint.config.cjs',
+      '**/playwright.config.ts',
+      '**/vite.config.ts',
+    ],
   },
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+    files: [
+      '**/*.ts',
+      '**/*.tsx',
+      '**/*.js',
+      '**/*.jsx',
+      '**/*.cjs',
+      '**/*.mjs',
+    ],
     rules: {
+      '@typescript-eslint/explicit-module-boundary-types': 'error',
+      '@typescript-eslint/explicit-function-return-type': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'no-console': 'warn',
+      'padding-line-between-statements': [
+        'error',
+        { blankLine: 'always', prev: '*', next: 'return' },
+        { blankLine: 'always', prev: '*', next: 'break' },
+        { blankLine: 'always', prev: 'block', next: '*' },
+        { blankLine: 'always', prev: 'block-like', next: '*' },
+        { blankLine: 'always', prev: 'if', next: '*' },
+      ],
       '@nx/enforce-module-boundaries': [
         'error',
         {
@@ -23,39 +51,6 @@ module.exports = [
           ],
         },
       ],
-    },
-  },
-  {
-    files: [
-      '**/*.ts',
-      '**/*.tsx',
-      '**/*.js',
-      '**/*.jsx',
-      '**/*.cjs',
-      '**/*.mjs',
-    ],
-    // Override or add rules here
-    rules: {
-      "@typescript-eslint/explicit-module-boundary-types": "error",
-      "@typescript-eslint/explicit-function-return-type": "error",
-      "@typescript-eslint/no-unused-vars": "error",
-      "no-console": "warn",
-      "padding-line-between-statements": [
-        "error",
-        { blankLine: "always", prev: "*", next: "return" },
-        { blankLine: "always", prev: "*", next: "break" },
-        { blankLine: "always", prev: "block", next: "*" },
-        { blankLine: "always", prev: "block-like", next: "*" },
-        { blankLine: "always", prev: "if", next: "*" },
-      ],
-      "@nx/enforce-module-boundaries": ["error", {
-        enforceBuildableLibDependency: true,
-        allow: [],
-        depConstraints: [{
-          sourceTag: "*",
-          onlyDependOnLibsWithTags: ["*"],
-        }],
-      }],
     },
   },
 ];
